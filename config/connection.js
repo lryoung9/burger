48e7dbb9c4555f0f30cf22fd3b0e132cad5cbe54
Lydia Young <lydiaryoung@gmail.com>
2018-03-05 20:22:44 -0700

    still trying to fix connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  })
+}
+
 var connection = mysql.createConnection({
   port: 3306,
   host: "gp96xszpzlqupw4k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
